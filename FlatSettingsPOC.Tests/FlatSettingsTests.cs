50525757225a259270a3c7fe26533620b9ae83a7
Agatino Pesce <agatino.pesce@gmail.com>
2024-02-14 22:03:06 +0100

    Tested a few configuration of growing complexity

--- FlatSettingsPOC.Tests/FlatSettingsTests.cs
+++ FlatSettingsPOC.Tests/FlatSettingsTests.cs
@@ -1,52 +1,117 @@
 using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
 
 namespace FlatSettingsPOC.Tests;
 
 public class FlatSettingsTests : IClassFixture<Fixture>
 {
-
-    private readonly FlatSettingsPOC.Tests.Fixture _fixture;
+    private readonly Fixture fixture;
 
     public FlatSettingsTests(Fixture fixture)
     {
-        _fixture = fixture;
+        this.fixture = fixture;
     }
 
     [Fact]
-    public void ShouldReadSimpleString() 
+    public void ShouldReadSimpleString()
     {
-    const string settingsKey = "TestKey";
-    const string settingsValue = "TestValue";
+        const string settingsKey = "TestKey";
+        const string settingsValue = "TestValue";
 
-    string actual = _fixture.Configuration.GetValue<string>(settingsKey)!;
+        string actual = fixture.Configuration.GetValue<string>(settingsKey)!;
         actual.Should().Be(settingsValue);
     }
-    
+
     [Fact]
     public void ShouldReadStringArray()
     {
-        const string sectionName= "SimpleStringArray";
+        const string sectionName = "SimpleStringArray";
         string[] expected = ["string0", "string1"];
-        
-        IConfigurationSection section = _fixture.Configuration.GetSection(sectionName)!;
+
+        IConfigurationSection section = fixture.Configuration.GetSection(sectionName)!;
         string[]? actual = section.Get<string[]>();
         actual.Should().BeEquivalentTo(expected);
     }
 
+    [Fact]
     public void ShouldReadComplexObject()
     {
-        const string sectionName = "TestSection";
-        string[] expected = ["string0", "string1"];
+        const string sectionName = "QueWithMappings";
+        const string expectedQueueName = "QueueName";
+        string[] expectedMappings = ["mapping0", "mapping1"];
 
-        IConfigurationSection section = _fixture.Configuration.GetSection(sectionName)!;
-        string[]? actual = section.Get<string[]>();
+        QueueWithMappings queueWithMappings = fixture.Configuration.GetSection(sectionName).Get<QueueWithMappings>()!;
+
+        queueWithMappings.Queue.Should().Be(expectedQueueName);
+        queueWithMappings.Mappings.Should().BeEquivalentTo(expectedMappings);
+    }
+
+    [Fact]
+    public void ShouldReadComplexFlatObject()
+    {
+        const string sectionName = "FlatQueueWithMappings";
+        const string expectedQueueName = "FlatQueueName";
+        string[] expectedMappings = ["FlatMapping0", "FlatMapping1"];
+
+        QueueWithMappings queueWithMappings = fixture.Configuration.GetSection(sectionName).Get<QueueWithMappings>()!;
+
+        queueWithMappings.Queue.Should().Be(expectedQueueName);
+        queueWithMappings.Mappings.Should().BeEquivalentTo(expectedMappings);
+    }
+
+    [Fact]
+    public void ShouldReadComplexFlatObjectArray()
+    {
+        const string section = "ManyFlatQueues";
+
+        QueueWithMappings[] expected = [
+            new QueueWithMappings()
+            {
+                Queue = "Queue0",
+                Mappings = ["Queue0Mapping0", "Queue0Mapping1"]
+            },
+            new()
+            {
+                Queue = "Queue1",
+                Mappings = ["Queue1Mapping0", "Queue1Mapping1"]
+            },
+        ];
+        QueueWithMappings[] actual =
+            fixture.Configuration.GetSection(section).Get<QueueWithMappings[]>()!;
+
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+
+    [Fact]
+    public void ShouldBindComplexFlatObjectList()
+    {
+        const string section = "ManyFlatQueues";
+
+        List<QueueWithMappings> expected = [
+            new QueueWithMappings()
+            {
+                Queue = "Queue0",
+                Mappings = ["Queue0Mapping0", "Queue0Mapping1"]
+            },
+            new()
+            {
+                Queue = "Queue1",
+                Mappings = ["Queue1Mapping0", "Queue1Mapping1"]
+            },
+        ];
+
+        List<QueueWithMappings> actual =new();
+        fixture.Configuration.GetSection(section).Bind(actual);
+
         actual.Should().BeEquivalentTo(expected);
     }
 }
 
 public class QueueWithMappings
 {
-    public string Queue { get;set; }
-    public string [] Mappings { get; set; }
-
+    [Required]
+    public string Queue { get; set; } = default!;
+    [Required]
+    public string[] Mappings { get; set; } = default!;
 }
